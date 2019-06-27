fbe47a769f7fdf39928f6316760da13679486dc2
raunaqdev <52268797+raunaqdev@users.noreply.github.com>
2019-06-27 09:45:50 +0530

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user  = {'username':'RAUNAQ'}
+    return '''
+    <html>
+    <head>
+      <title>HOME PAGE - MICROBLOG</title>
+        </head>
+        <body bgcolor = 'blue'>
+        <h1>Hello, ''' + user['username'] + '''</h1>
+         </body>
+         </html>'''
+    
